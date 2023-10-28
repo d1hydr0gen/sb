9cb42a303fdd082b0292a9f8e7a94fd5a712afbf
一酸化二水素君 <142431978+d1hydr0gen@users.noreply.github.com>
2023-10-28 23:49:01 +0900

    Update rp_schema.py

--- src/rp_schema.py
+++ src/rp_schema.py
@@ -18,7 +18,7 @@ INPUT_SCHEMA = {
         'type': int,
         'required': False,
         'default': 512,
-        'constraints': lambda height: height in [128, 256, 384, 448, 512, 576, 640, 704, 768]
+        'constraints': lambda height: height in [128, 256, 384, 448, 512, 576, 640, 704, 768,800,1024]
     },
     'init_image': {
         'type': str,
